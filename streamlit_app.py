da15512e14e81dc06582b16acbb73c0623bddde8
martmezo <martmezo11@gmail.com>
2024-06-11 21:09:12 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 # Import python packages
 import streamlit as st;
 
-from snowflake.snowpark.functions 
-import col
+from snowflake.snowpark.functions import session
 
 # Write directly to the app
 st.title("Customize your Smoothie!")
